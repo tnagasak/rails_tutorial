0e9a7c30fb9f033e16ceb0cf9c8da5a4bde22320
tomoya.nagasaki <n.agasakito@gmail.com>
2024-04-21 22:34:27 +0900

    tmp

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -28,9 +28,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
-  test "should get content" do
-    get static_pages_content_url
+  test "should get contact" do
+    get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Content | #{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
