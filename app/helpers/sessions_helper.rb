a9a002599a9a4c9a511ea188832fc5f13b18625c
tomoya.nagasaki <n.agasakito@gmail.com>
2024-05-08 02:01:01 +0900

    tmp0508

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,36 @@
+module SessionsHelper
+    # 渡されたユーザーでログインする
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    # before refactor 1
+    # def current_user
+    #     if session[:user_id]
+    #       User.find_by(id: session[:user_id])
+    #     end
+    # end
+    
+    # before refactor 2
+    # def current_user
+    #     if session[:user_id]
+    #       if @current_user.nil?
+    #           @current_user = User.find_by(id: session[:user_id])
+    #       else
+    #           @current_user
+    #       end
+    #     end
+    # end
+
+    # 現在ログイン中のユーザーを返す (いる場合)
+    def current_user
+        if session[:user_id]
+          @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+
+    # ユーザーがログインしていればtrue、その他ならfalseを返す
+    def logged_in?
+        !current_user.nil?
+    end
+end
