3cc9e0d2f73975b531fb2e9363cce1e259f1216f
tomoya.nagasaki <n.agasakito@gmail.com>
2024-05-09 23:58:10 +0900

    finish 8

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  # rails7のエラー解消のため追加
+  get '/logout', to: 'sessions#destroy'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
