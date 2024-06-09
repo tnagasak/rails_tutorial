dc6c8ee942a0d6c4af6bee2c77f6cf0252b58594
tomoya.nagasaki <n.agasakito@gmail.com>
2024-06-09 22:52:23 +0900

    finish 11

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_09_031000) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_09_033432) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -20,6 +20,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_09_031000) do
     t.string "occupation"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
